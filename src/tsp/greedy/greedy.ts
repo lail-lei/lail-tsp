4164ad8e91f0d1c1b47b76dd68e20d43e26caf0f
lail <lforoura@gmail.com>
2023-06-02 18:58:42 +0000

    add nearest neighbor insertion

---
+++ src/tsp/greedy/greedy.ts
@@ -0,0 +1,41 @@
+
+export class GreedyHeuristics {
+    distances: Matrix;
+
+    constructor(distances: Matrix) {
+        this.distances = distances;
+    }
+
+    nearestNeighborInsertionPath = () => {
+
+        const stack: Array<number> = [2];
+        const path: Array<number> = [0, 1];
+        const visited = new Set<number>([0, 1]);
+
+        const findNearestNeighborIndex = (vertex: number) => {
+            let min = Infinity;
+            let minIndex = -1;
+            for (let c = 0; c < this.distances[0].length + 1; c++) {
+                if (vertex === c || visited.has(c)) continue;
+                if (this.distances[vertex][c] <= min) {
+                    minIndex = c;
+                    min = this.distances[vertex][minIndex];
+                }
+            }
+            return minIndex;
+        }
+
+        while (stack.length > 0) {
+            const current = stack.pop();
+            if (current === undefined) continue;
+            path.push(current);
+            const next = findNearestNeighborIndex(current);
+            // encountered all, so break
+            if (next === -1) break;
+            visited.add(current);
+            stack.push(next);
+        }
+
+        return path;
+    }
+}
