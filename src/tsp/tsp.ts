1e9ab97253dde53dca6c313e5e7f11d3fb76d7a9
lail <lforoura@gmail.com>
2023-06-23 17:37:13 +0000

    create private methods and code clean up

--- src/tsp/tsp.ts
+++ src/tsp/tsp.ts
@@ -8,17 +8,16 @@ export interface PathResult {
 }
 
 export class TSP {
-  nodes: PathNode[];
-  start: PathNode;
-  end?: PathNode | null;
-  costMatrix: number[][];
-  floorPlan: number[][]; // wall data
-  // contains predetermined start, end + dummy nodes
-  allNodes: PathNode[];
-  greedy?: GreedyHeuristics;
-  mst?: MSTHeuristics;
+  private nodes: PathNode[];
+  private start: PathNode;
+  private costMatrix: number[][];
+  private floorPlan: number[][]; // for wall data
+  private allNodes: PathNode[]; // contains predetermined start, end + dummy nodes
+  private greedy: GreedyHeuristics;
+  private mst: MSTHeuristics;
+  private end?: PathNode | null;
   error?: string;
-  distanceHeuristic?: DistanceHeuristic;
+  private distanceHeuristic?: DistanceHeuristic;
 
   constructor({
     nodes,
@@ -26,12 +25,14 @@ export class TSP {
     start,
     end,
     distanceHeuristic,
+    precalculatedDistances,
   }: {
     nodes: PathNode[];
     floorplan: number[][];
     start: PathNode;
     end?: PathNode;
     distanceHeuristic?: DistanceHeuristic;
+    precalculatedDistances?: number[][];
   }) {
     this.floorPlan = floorplan;
     this.start = start;
@@ -39,24 +40,26 @@ export class TSP {
     this.distanceHeuristic = distanceHeuristic;
     this.nodes = nodes;
     this.allNodes = end && this.isHamiltonianPathProblem() ? [start, end, ...nodes] : [start, ...nodes];
-    this.costMatrix = new Array(this.allNodes.length)
-      .fill(false)
-      .map(() => new Array(this.allNodes.length).fill(Infinity));
-  }
+    if (precalculatedDistances)
+      this.costMatrix = precalculatedDistances;
+    else {
+      this.costMatrix = new Array(this.allNodes.length)
+        .fill(false)
+        .map(() => new Array(this.allNodes.length).fill(Infinity));
+      this.calculateDistances();
+    }
 
-  init = () => {
-    this.calculateDistances();
     this.greedy = new GreedyHeuristics(this.costMatrix, this.isHamiltonianPathProblem());
     this.mst = new MSTHeuristics(this.costMatrix, this.isHamiltonianPathProblem());
-  };
+  }
 
-  isHamiltonianPathProblem = () => !!(this.end && this.end.gridId !== this.start.gridId);
+  /** Constructor helpers */
 
-  computeCostMatrix = (rowIndex: number, columnIndex: number) => {
+  private calculateDistances = () => {
     const aStar = new AStar(this.floorPlan);
 
-    for (let r = rowIndex; r < this.allNodes.length; r++) {
-      for (let c = columnIndex; c < this.allNodes.length; c++) {
+    for (let r = 0; r < this.allNodes.length; r++) {
+      for (let c = 0; c < this.allNodes.length; c++) {
         if (r === c) this.costMatrix[r][c] = 0;
         else {
           const start = this.allNodes[r];
@@ -71,10 +74,6 @@ export class TSP {
         }
       }
     }
-  };
-
-  calculateDistances = () => {
-    this.computeCostMatrix(0, 0);
     // hamiltonian path problem requires a specified start and end
     if (this.isHamiltonianPathProblem()) {
       this.costMatrix[0][1] = Infinity;
@@ -82,63 +81,35 @@ export class TSP {
     }
   };
 
-  transformRawPath = (rawPath: number[]): PathNode[] => rawPath.map((index: number) => this.allNodes[index]);
 
-  estimateTotalPathCost = (rawPath: number[]): number => {
-    if (this.error) throw new Error(this.error);
-    if (rawPath.length === 1) return rawPath[0];
+  private isHamiltonianPathProblem = () => !!(this.end && this.end.gridId !== this.start.gridId);
 
-    let sum = 0;
-    for (let i = 0; i < rawPath.length - 2; i++) {
-      const vertexA = rawPath[i];
-      const vertexB = rawPath[i + 1];
-      const cost = this.costMatrix[vertexA][vertexB];
-      sum += cost;
-    }
-    return sum;
-  };
 
-  connectBackToStart = (path: PathNode[]) => {
-    if (this.error) throw new Error(this.error);
-    return [...path, this.start];
-  };
-
-  computePath = (computePath: () => number[]): PathResult => {
-    // todo - if list is sufficiently short, return list without computing
-    const rawPath = computePath();
-    const transformed = this.transformRawPath(rawPath);
-    const path = this.isHamiltonianPathProblem() ? transformed : this.connectBackToStart(transformed);
-    return { path, estimatedCost: this.estimateTotalPathCost(rawPath) };
-  };
+  /** Main (Path) Methods */
 
   nearestNeighborPath = (): PathResult => {
     if (this.error) throw new Error(this.error);
-    if (this.greedy === undefined) throw new Error('must call TSP.init() before calculating paths');
     return this.computePath(this.greedy.nearestNeighborPath);
   };
 
   nearestInsertionPath = (): PathResult => {
     if (this.error) throw new Error(this.error);
-    if (this.greedy === undefined) throw new Error('must call TSP.init() before calculating paths');
     return this.computePath(this.greedy.nearestInsertionPath);
   };
 
   farthestInsertionPath = (): PathResult => {
     if (this.error) throw new Error(this.error);
-    if (this.greedy === undefined) throw new Error('must call TSP.init() before calculating paths');
     return this.computePath(this.greedy.farthestInsertionPath);
   };
 
-  christofides = (): PathResult => {
+  private christofides = (): PathResult => {
     if (this.error) throw new Error(this.error);
-    if (this.mst === undefined) throw new Error('must call TSP.init() before calculating paths');
     if (this.isHamiltonianPathProblem()) throw new Error('Christofides requires a complete graph to work');
     return this.computePath(this.mst.christofides);
   };
 
   alphanumericSort = (): PathResult => {
     if (this.error) throw new Error(this.error);
-    if (this.greedy === undefined) throw new Error('must call TSP.init() before calculating paths');
     const sorted = this.nodes.sort((nodeA: PathNode, nodeB: PathNode) => {
       if (nodeA.uid && nodeB.uid) return nodeA.uid.localeCompare(nodeB.uid);
       return nodeA.gridId.localeCompare(nodeB.gridId);
@@ -152,4 +123,37 @@ export class TSP {
     );
     return { path, estimatedCost: this.estimateTotalPathCost(rawPath) };
   };
+
+
+  /** Main (Path) Methods helpers */
+  private computePath = (computePath: () => number[]): PathResult => {
+    // todo - if list is sufficiently short, return list without computing
+    const rawPath = computePath();
+    const transformed = this.transformRawPath(rawPath);
+    const path = this.isHamiltonianPathProblem() ? transformed : this.connectBackToStart(transformed);
+    return { path, estimatedCost: this.estimateTotalPathCost(rawPath) };
+  };
+
+  private transformRawPath = (rawPath: number[]): PathNode[] => rawPath.map((index: number) => this.allNodes[index]);
+
+  private estimateTotalPathCost = (rawPath: number[]): number => {
+    if (this.error) throw new Error(this.error);
+    if (rawPath.length === 1) return rawPath[0];
+
+    let sum = 0;
+    for (let i = 0; i < rawPath.length - 2; i++) {
+      const vertexA = rawPath[i];
+      const vertexB = rawPath[i + 1];
+      const cost = this.costMatrix[vertexA][vertexB];
+      sum += cost;
+    }
+    return sum;
+  };
+
+  private connectBackToStart = (path: PathNode[]) => {
+    if (this.error) throw new Error(this.error);
+    return [...path, this.start];
+  };
+
+
 }
